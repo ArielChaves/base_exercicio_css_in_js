cf1f1c26becf6086d0c3fce362247cce74165fd6
ArielChaves <139404966+ArielChaves@users.noreply.github.com>
2025-07-17 14:22:57 -0300

    style.ts

--- src/components/FormVagas/style.ts
+++ src/components/FormVagas/style.ts
@@ -1,26 +1,26 @@
 import styled from 'styled-components'
 
-export const FormPesquisa = styled.form`
+export const Form = styled.form`
   display: grid;
   grid-template-columns: 1fr auto;
-  background-color: ${(props) => props.theme.corSecundaria};
+  background-color: #f9f5e7;
   padding: 32px;
   border-radius: 12px;
   margin-top: 40px;
 `
 
-export const ButtonPesquisar = styled.button`
-  background-color: ${(props) => props.theme.corPrincipal};
-  border: 1px solid ${(props) => props.theme.corPrincipal};
+export const ButtonSearch = styled.button`
+  background-color: #a7727d;
+  border: 1px solid #a7727d;
   height: 40px;
   padding: 0 16px;
   font-size: 18px;
-  color: ${(props) => props.theme.corSecundaria};
+  color: #f9f5e7;
   margin-left: 8px;
   cursor: pointer;
 `
 
-export const InputPesquisar = styled.input`
+export const Campo = styled.input`
   padding: 0 16px;
-  outline-color: ${(props) => props.theme.corPrincipal};
+  outline-color: #a7727d;
 `
